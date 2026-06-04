9e6bf8f67ab942377a953014b5dc40b0dc46a09b
Prajwal Meti <prajwalmeti25@gmail.com>
2026-06-04 18:37:54 +0530

    Add 2D graphics editor with canvas functions
    Implemented a simple 2D graphics editor with canvas display and clear functionality.

--- main.c
+++ main.c
@@ -1,1 +1,77 @@
+#include <stdio.h>
+
+#define ROWS 30
+#define COLS 60
+
+char canvas[ROWS][COLS];
+
+/* Function Prototypes */
+void clearCanvas();
+void displayCanvas();
+
+/* Clear the canvas */
+void clearCanvas()
+{
+    for(int i = 0; i < ROWS; i++)
+    {
+        for(int j = 0; j < COLS; j++)
+        {
+            canvas[i][j] = ' ';
+        }
+    }
+}
+
+/* Display the canvas */
+void displayCanvas()
+{
+    printf("\n===== CANVAS =====\n");
+
+    for(int i = 0; i < ROWS; i++)
+    {
+        for(int j = 0; j < COLS; j++)
+        {
+            printf("%c", canvas[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int choice;
+
+    clearCanvas();
+
+    do
+    {
+        printf("\n===== 2D GRAPHICS EDITOR =====\n");
+        printf("1. Display Canvas\n");
+        printf("2. Clear Canvas\n");
+        printf("3. Exit\n");
+        printf("Enter Choice: ");
+        scanf("%d", &choice);
+
+        switch(choice)
+        {
+            case 1:
+                displayCanvas();
+                break;
+
+            case 2:
+                clearCanvas();
+                printf("Canvas Cleared Successfully!\n");
+                break;
+
+            case 3:
+                printf("Exiting Program...\n");
+                break;
+
+            default:
+                printf("Invalid Choice!\n");
+        }
+
+    } while(choice != 3);
+
+    return 0;
+}
 
